f8e9cde682d075b498d9b7417fcf4405bfcd346c
Dr Nic Williams <drnicwilliams@gmail.com>
2018-06-14 12:58:37 +1000

    fetches LSALT grid data from backend API on start

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import * as fs from 'fs';
 import * as URL from 'url';
 import * as _ from 'underscore';
 import * as cfenv from 'cfenv';
+// import * as turf from 'turf';
 
 import * as request from 'request';
 
@@ -19,16 +21,26 @@ if (process.env.BACKEND_URL) {
 }
 console.log(`Requests on path ${appURLPath}`);
 
+let gafAreaCodes = ["WA-N", "WA-S", "NT", "QLD-N", "QLD-S", "SA", "NSW-W", "NSW-E", "VIC", "TAS"];
+
+interface LSALTGrid {
+  grid: number[][];
+  lsalt_100ft: number;
+}
+let lsaltData: { [gafAreaCode: string]: LSALTGrid[] } = {}
+
+gafAreaCodes.forEach(gafAreaCode => {
+  let url = _.clone(mainAppURL);
+  url.pathname = `/json/lsalt-${gafAreaCode}.json`;
+
+  request(URL.format(url), (error, response, body) => {
+    console.log(gafAreaCode);
+    lsaltData[gafAreaCode] = JSON.parse(body);
+  });
+});
 
 app.get(appURLPath, function (req, res: express.Response) {
-  let url = _.clone(mainAppURL);
-  url.pathname = "/json/lsalt-TAS.json";
-  request(URL.format(url))
-    .on('error', (err) => {
-      console.log(err);
-      res.json({"error": "connecting to backend API"});
-    })
-    .pipe(res);
+  res.json(lsaltData['WA-S']);
 })
 
 // locally provide $PORT
